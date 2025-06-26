76453e8ea4b36d4010222856ef15fc14ddf2c0d1
Danila <kallaurdanila13@gmail.com>
2025-06-26 22:33:23 +0300

    penis3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const startBtn = document.querySelector('#start')
 const screens = document.querySelectorAll('.screen')
 const timeList = document.querySelector('#time-list')
 const timeEl = document.querySelector('#time')
-let time = 20
+let time = 11
 
 startBtn.addEventListener('click', (event) => {
     event.preventDefault()
@@ -19,7 +19,26 @@ timeList.addEventListener('click', event => {
 
 })
 
+startGame()
 
-function startGame(){   
-    timeEl.innerHTML = `00:${time}`
+function startGame(){
+    setInterval(decreaseTime, 1000)   
+    setTime(time)
+}
+
+function decreaseTime(){
+    if(time === 0){
+
+    }else{
+    let current = --time
+    if(current < 10){
+        current = `0${current}`
+    }
+    setTime(current)
+    }
+    
+}
+
+function setTime(value){
+    timeEl.innerHTML = `00:${value}`
 }
